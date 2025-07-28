eeb4676d1d99b3b5b2829120f222cab6218df457
Thimoss <thimotipandia4880@gmail.com>
2025-07-28 17:17:38 +0700

    update

--- src/sector/sector.service.ts
+++ src/sector/sector.service.ts
@@ -1,11 +1,40 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { CreateSectorDto } from './dto/create-sector.dto';
 import { UpdateSectorDto } from './dto/update-sector.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class SectorService {
-  create(createSectorDto: CreateSectorDto) {
-    return 'This action adds a new sector';
+  constructor(private readonly prisma: PrismaService) {}
+  async create(createSectorDto: CreateSectorDto) {
+    const { no, name, source, categoryId } = createSectorDto;
+
+    // Cek apakah sudah ada sektor dengan 'no' yang sama dalam kategori yang sama
+    const existingSector = await this.prisma.sector.findUnique({
+      where: {
+        no_categoryId: {
+          no,
+          categoryId, // Menggunakan kombinasi 'no' dan 'categoryId'
+        },
+      },
+    });
+
+    if (existingSector) {
+      throw new HttpException(
+        `Sector with no '${no}' already exists in this category.`,
+        HttpStatus.FOUND,
+      );
+    }
+
+    // Jika tidak ada sektor dengan 'no' yang sama, lanjutkan untuk membuat sektor baru
+    return await this.prisma.sector.create({
+      data: {
+        no,
+        name,
+        source,
+        categoryId,
+      },
+    });
   }
 
   findAll() {
